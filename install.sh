491e67bba940cfa0997700df0eace1598103fbfe
Leonardo Arias Fonseca (¡paz y baile!) <yo@elopio.net>
2016-10-04 22:42:53 -0600

    Add my personal email configuration

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ dir="$(dirname $0)"
 # email
 ln -s "$dir/email/offlineimap/.offlineimaprc" "$HOME/.offlineimaprc"
 ln -s "$dir/email/msmtp/.msmtprc" "$HOME/.msmtprc"
+# mutt
 ln -s "$dir/email/mutt/.muttrc" "$HOME/.muttrc"
+mkdir -p "$HOME/.mutt/accounts"
+ln -s "$dir/email/mutt/accounts/personal" "$HOME/.mutt/accounts/personal"
+ln -s "$dir/email/mutt/accounts/canonical" "$HOME/.mutt/accounts/canonical"
