e42e6be1ee9d898f8557adb2746107daedf5e62f
Leo Arias <leo.arias@canonical.com>
2017-07-17 21:19:23 +0000

    forgot the reference

--- gnome/terminal_bell.sh
+++ gnome/terminal_bell.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Disable the audible bell and enable the visual one.
+#
+# Thanks to A.B. for: https://askubuntu.com/a/620053/61416
 
 gsettings set org.gnome.desktop.wm.preferences audible-bell false
 gsettings set org.gnome.desktop.wm.preferences visual-bell true
