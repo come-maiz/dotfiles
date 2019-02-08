b1a3924ae27f54905168dce8b3930d7c5c40cfd7
Leo Arias <elopio@calchas.lan>
2019-02-08 08:39:29 -0600

    Add agenda config to emacs

--- editors/emacs/.emacs.d/init.el
+++ editors/emacs/.emacs.d/init.el
@@ -44,4 +44,20 @@
 
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
+(setq org-agenda-files (list "~/Sync/to-do.org"))
+
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "MEETING"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "blue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold))))
+(setq org-agenda-window-setup 'current-window)
+
 (provide 'init)
